dddaf270ce08bdd7e95419e4b6f717dee092b001
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-05-06 19:37:36 +0200

    did some changes here but cant test it yet

--- Algorithms-And-Data-Structures/TurboCollections/TurboSort.cpp
+++ Algorithms-And-Data-Structures/TurboCollections/TurboSort.cpp
@@ -2,7 +2,24 @@
 
     std::vector<int> TurboCollections::TurboSort::TurboList()
     {
-        std::vector<int> lists {1,2,3,4};
+        std::vector<int> lists {4,2,3,1};
+        int n = lists.size();
+
+        for(int i = 0; i < n-2; i++)
+        {
+            int min = i;
+            for(int j = i + 1; j < n - 1; j++)
+            {
+                if (lists[j] < lists[min])
+                {
+                   min = j;
+                }
+            }
+            if (min != i)
+            {
+                std::swap(lists[min], lists[i]);
+            }
+        }
         return lists;
     }
 
