0cf18da512a461306fb92aa19e4d56a3be5dbb9e
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-05-10 21:07:32 +0200

    something is working

--- Algorithms-And-Data-Structures/GameStateHistory/GameStateHistory.cpp
+++ Algorithms-And-Data-Structures/GameStateHistory/GameStateHistory.cpp
@@ -25,12 +25,41 @@ public:
         nextNode->Previous = lastNode;
         lastNode = nextNode;
     }
+
+    void backChoice(const T& backState)
+    {
+        Node* backNode = new Node(backState);
+        backNode->Value = lastNode->Previous;
+        lastNode = backNode;
+        cout<<lastNode;
+    }
+
+  static void level1(){cout<<"Go to Level 1";}
+  static void level2(){cout<<"Go to Level 2";}
+  static void settings(){cout<<"Go to Settings";}
+  static void goBack(){cout<<"You went back to ";}
+  static void quitGame(){cout<<"Game quitting..";}
+
+    void choose()
+    {
+        char type;
+        cin >> type;
+        switch (type)
+        {
+        case '1':level1();break;
+        case '2':level2();break;
+        case '0':settings();break;
+        case 'b':goBack();break;
+        case 'q':quitGame();break;
+        }
+    }
 };
 
 int main(int argc, char* argv[])
 {
     GameStateHistory<string> stack;
     stack.inGame("MainMenu");
+    stack.choose();
     
     return 0;
 }
