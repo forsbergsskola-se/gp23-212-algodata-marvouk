928a43744d4dccccb0411750b002846af971169e
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-05-13 14:33:23 +0200

    created average.h and unit test

---
+++ Algorithms-And-Data-Structures/TurboCollectionsTest/AverageAlgorithmsTest.cpp
@@ -0,0 +1,13 @@
+#include "TurboLinkedStack.h"
+#include "TurboMaths.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+
+namespace TurboCollections
+{
+    namespace AverageAlgorithmsTest
+    {
+        
+    }
+}
