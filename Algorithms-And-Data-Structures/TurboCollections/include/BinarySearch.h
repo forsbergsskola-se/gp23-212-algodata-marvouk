cc8d5107ba3e3b3f5174354f2f05c25359e8026e
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-05-20 12:10:42 +0200

    started binary search

---
+++ Algorithms-And-Data-Structures/TurboCollections/include/BinarySearch.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+
+class BinarySearch
+{
+    BinarySearch() = delete;
+public:
+   static int binary_search(std::vector<int>& a, int n, int x)
+   {
+       
+       return 0;
+   }
+};
