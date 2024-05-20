44f8b39e7bee6a9f73e2ab0eef0e4631d98e0827
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-05-20 12:31:00 +0200

    renamed binary search tree and created binary search class

--- Algorithms-And-Data-Structures/TurboCollections/include/BinarySearch.h
+++ Algorithms-And-Data-Structures/TurboCollections/include/BinarySearch.h
@@ -1,13 +1,39 @@
 #pragma once
 #include <vector>
 
-class BinarySearch
+namespace TurboCollections
 {
-    BinarySearch() = delete;
-public:
-   static int binary_search(std::vector<int>& a, int n, int x)
-   {
-       
-       return 0;
-   }
-};
+    template <typename T>
+    class BinarySearch
+    {
+        BinarySearch() = delete;
+    public:
+        static int binary_search(std::vector<T>& a, int& n, int x)
+        {
+            n = a.size();
+            int lowerBound = 0;
+            int upperBound = n - 1;
+
+            while(x == NULL)
+            {
+                if(upperBound < lowerBound)
+                {
+                    return -1 ;
+                }
+                int midPoint = (lowerBound + upperBound)/2;
+         
+                if(a[midPoint] < x)
+                {
+                    lowerBound = midPoint +1;
+                }else if(a[midPoint] > x)
+                {
+                    upperBound = midPoint - 1;
+                }else if(a[midPoint] = x)
+                {
+                    return midPoint;
+                }         
+            }
+        }
+    };
+}
+    
---
+++ Algorithms-And-Data-Structures/TurboCollectionsTest/TurboBinarySearchTest.cpp
@@ -0,0 +1,13 @@
+#include "..\TurboCollections\include\BinarySearch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+namespace TurboCollections
+{
+ 
+       TEST(BinarySearch, TestingIfWorking)
+       {
+          
+       }
+    
+}
