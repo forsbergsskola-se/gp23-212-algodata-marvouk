893edbf32807004526ca45ade571b32708da7a48
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-05-20 17:23:04 +0200

    changed everything on binary tree

--- Algorithms-And-Data-Structures/TurboCollections/include/TurboBinaryTree.h
+++ Algorithms-And-Data-Structures/TurboCollections/include/TurboBinaryTree.h
@@ -3,25 +3,56 @@
 #include <vector>
 
 template <typename T>
-struct Tree                  //define Tree<T>
+class Tree                  //define Tree<T>
 {
     T data;                //value : T
     Tree* Left;     // left : Tree<T>
     Tree* Right;     // right : Tree<T>
-
+    
     Tree(T value) : data(value), Left(nullptr), Right(nullptr) {}//constr
     
-    static Tree* CreateNode(T data)
+    
+    static Tree*  GetLeftChild( T n)
     {
-        return new Tree(data);
+        return n * 2 + 1;
     }
- 
+
+    static Tree* GetRightChild(T n)
+    {
+        return n * 2 + 2;
+    }
+
+    static Tree* GetValue(T n)
+    {
+     return data[n]; // return nodes at index n
+    }
+
+    static Tree* SetValue(T n,T value)
+    {
+       value = data[n] ; //set nodes at index n to value
+       return value;
+    }
 };
 
 
-/*template <typename T>
-class Node<T>
+template <typename T>
+struct Node
 {
+    T index = 0;
+    Tree<T>* tree = new Tree<T>;
     
-};*/
+    static Node* GetLeftChild()
+    {
+        return new Node(tree->GetLeftChild(index));
+    }
+   static Node* GetValue()
+    {
+      return tree->GetValue(index);
+    }
+
+    static Node*  SetValue(T value)
+    {
+        return tree.SetValue(index, value);
+    }
+};
 
--- Algorithms-And-Data-Structures/TurboCollectionsTest/TurboBinaryTreeTest.cpp
+++ Algorithms-And-Data-Structures/TurboCollectionsTest/TurboBinaryTreeTest.cpp
@@ -8,19 +8,7 @@ namespace TurboCollections
        TEST(TestBinaryTree, TestingIfWorking)
        {
            Tree<int>* newTree = Tree<int>::CreateNode(0);
-           newTree->CreateNode(0);
-           EXPECT_EQ(newTree->data,0);
-           
-           newTree->Left = Tree<int>::CreateNode(1);
-           EXPECT_EQ(newTree->Left->data,1);
-
-           newTree->Left->Left = Tree<int>::CreateNode(3);
-           EXPECT_EQ(newTree->Left->Left->data,3);
-
-           newTree->Right = Tree<int>::CreateNode(2);
-           EXPECT_EQ(newTree->Right->data,2);
-
-           delete newTree; 
+          
        }
     
 }
