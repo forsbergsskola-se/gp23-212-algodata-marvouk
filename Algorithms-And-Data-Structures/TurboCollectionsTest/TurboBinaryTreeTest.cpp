2d2ed9d77a6b887da0b51c336d3f24f275839ed0
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-05-21 18:34:30 +0200

    left and right they might working now too

--- Algorithms-And-Data-Structures/TurboCollectionsTest/TurboBinaryTreeTest.cpp
+++ Algorithms-And-Data-Structures/TurboCollectionsTest/TurboBinaryTreeTest.cpp
@@ -22,6 +22,21 @@ namespace TurboCollections
 
             tree.SetValue(0,10);
             EXPECT_EQ(tree.GetValue(0),10);
+
+            tree.SetValue(0,1);
+            EXPECT_EQ(tree.GetLeftChild(0),1);
+        }
+
+        TEST(BinaryTree,TestRightChild)
+        {
+            Tree<int> tree;
+            tree.nodes = std::vector<int>(10);
+
+            tree.SetValue(0,10);
+            EXPECT_EQ(tree.GetValue(0),10);
+
+            tree.SetValue(0,2);
+            EXPECT_EQ(tree.GetRightChild(0),2);
         }
     }
     
