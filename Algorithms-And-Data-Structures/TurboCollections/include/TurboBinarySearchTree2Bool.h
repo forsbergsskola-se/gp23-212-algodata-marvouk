dc97d3bab66c4db321f84fb4a013d63460aa608a
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-05-28 00:11:39 +0200

    delete function is working not my solution

--- Algorithms-And-Data-Structures/TurboCollections/include/TurboBinarySearchTree2Bool.h
+++ Algorithms-And-Data-Structures/TurboCollections/include/TurboBinarySearchTree2Bool.h
@@ -56,11 +56,59 @@ struct Node
         return right->Search(value);
     }
 
-    bool Delete(T value)
-    {
-        if(value == data)return true;
-        if()
-        return false;
+    bool Delete(T value) {
+        Node* parent = nullptr;
+        Node* current = this;
+
+        // Find the node to be deleted and its parent
+        while (current != nullptr && current->data != value) {
+            parent = current;
+            if (value < current->data) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+
+        // If the node is not found
+        if (current == nullptr) {
+            return false;
+        }
+
+        // Case 1: Node with only one child or no child
+        if (current->left == nullptr || current->right == nullptr) {
+            Node* newCurr;
+
+            if (current->left == nullptr) {
+                newCurr = current->right;
+            } else {
+                newCurr = current->left;
+            }
+
+            // If the node to be deleted is the root node
+            if (parent == nullptr) {
+                this->data = newCurr ? newCurr->data : 0;
+                this->left = newCurr ? newCurr->left : nullptr;
+                this->right = newCurr ? newCurr->right : nullptr;
+                delete current;
+                return true;
+            }
+
+            // Set the parent to the new child
+            if (current == parent->left) {
+                parent->left = newCurr;
+            } else {
+                parent->right = newCurr;
+            }
+            delete current;
+        } else {
+            // Case 2: Node with two children
+            Node* successor = FindMin(current->right);
+            T successorValue = successor->data;
+            Delete(successor->data);
+            current->data = successorValue;
+        }
+        return true;
     }
 
 
